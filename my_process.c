5f080792f950a4d0982f57dfdd1c66b4b8dd382e
yvecca <justin881110@gmail.com>
2020-05-01 22:14:42 +0800

    fix

--- my_process.c
+++ my_process.c
@@ -35,7 +35,6 @@ int process_execute(struct process proc){
     }
     if(pid == 0){
         printf("fork\n");
-        /*
         usleep(1000);
         long start_t,end_t;
         start_t = syscall(get_time);
@@ -43,7 +42,7 @@ int process_execute(struct process proc){
             _unit_();
         }
         end_t = syscall(get_time);
-        //syscall(printk,getpid(),start_t,end_t)*/
+        //syscall(printk,getpid(),start_t,end_t)
         exit(0);
     }
     process_assign_CPU(pid,child_CPU);
